2ab3e7f6cdcc8193400ad7c102c3117fdceb4f3a
Rajat Sharan <errajatsharan@gmail.com>
2022-05-21 23:52:42 +0530

    Handing Async promises with Cypress

--- cypress/integration/example/FirstTest.spec.js
+++ cypress/integration/example/FirstTest.spec.js
@@ -1,17 +1,25 @@
 /// <reference types="Cypress" />
 
 
-describe('My First Test Suit', () => {
-    it('My First Test Case', () => {
+
+describe('My First Test Suit',function(){
+
+    it('My First Test Case', function() {
       cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
       cy.get('.search-keyword').type('ca')
       cy.wait(2000)
       cy.get('.product:visible').should('have.length',4)
       //Parent child chaining
-      cy.get('.products').find('.product').should('have.length',4)
-      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+      cy.get('.products').as('productlocator')// Using alias to reuse same locator
+      cy.get('@productlocator').find('.product').should('have.length',4)
+      cy.get('@productlocator').find('.product').eq(2).contains('ADD TO CART').click().then(function()
+      {
 
-      cy.get('.products').find('.product').each(($el, index, $list) => {
+        console.log("asdasdasd")
+
+      })
+
+      cy.get('@productlocator').find('.product').each(($el, index, $list) => {
         // $el is a wrapped jQuery element
       const textVeg=$el.find('h4.product-name').text()
       if(textVeg.includes('Cashews'))
@@ -20,17 +28,17 @@ describe('My First Test Suit', () => {
        cy.wrap($el).find('button').click()
 
       }
-          
 
+    })
+      //Assert to check logo text is correctly displaying or not
+
+    cy.get('.brand').should('have.text',"GREENKART")
+          
+    //this is to print in logs
      cy.get('.brand').then(function(logoelement){
 
       cy.log(logoelement.text())
 
-     })
-
-     
-   
-        
-      })
+    })
     })
   })
