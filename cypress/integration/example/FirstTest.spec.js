f4541f0af09d4cdd89d7fabf44fe71dda911d0e3
Rajat Sharan <errajatsharan@gmail.com>
2022-05-21 18:19:27 +0530

    Understanding get and find commands with Cypress

--- cypress/integration/example/FirstTest.spec.js
+++ cypress/integration/example/FirstTest.spec.js
@@ -1,12 +1,15 @@
 /// <reference types="Cypress" />
 
 
-describe('My First Test', () => {
-    it('Visits the Kitchen Sink', () => {
+describe('My First Test Suit', () => {
+    it('My First Test Case', () => {
       cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
       cy.get('.search-keyword').type('ca')
       cy.wait(2000)
       cy.get('.product:visible').should('have.length',4)
+      //Parent child chaining
+      cy.get('.products').find('.product').should('have.length',4)
+      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
     })
   })
   
