f57c5812870a92d8a9c1c409f350e4f3a7512e8c
Rajat Sharan <errajatsharan@gmail.com>
2022-05-21 17:11:25 +0530

    Basic Assertion in writing the tests with Cypress

--- cypress/integration/example/FirstTest.spec.js
+++ cypress/integration/example/FirstTest.spec.js
@@ -1,6 +1,12 @@
+/// <reference types="Cypress" />
+
+
 describe('My First Test', () => {
     it('Visits the Kitchen Sink', () => {
-      cy.visit('https://example.cypress.io')
+      cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+      cy.get('.search-keyword').type('ca')
+      cy.wait(2000)
+      cy.get('.product').should('have.length',4)
     })
   })
   
